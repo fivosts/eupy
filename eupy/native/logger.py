7ad592a4e1f16e17211122a8deea73744244f911
fivosts <fivos_ts@hotmail.com>
2020-03-11 17:36:44 +0000

    Convert logger to wrapper class

--- eupy/native/logger.py
+++ eupy/native/logger.py
@@ -1,29 +1,59 @@
 #!/usr/bin/env python
 import logging
 
-def configLogger(name):
+"""
+Wrapper class over logging.Logger to automate formatting and other jobs
+"""
+class Logger:
+
+	def __init__(self, name):
+		self.configLogger(name)
+		return
+
+	def _configLogger(self, name):
+		
+		# create logger
+		self._logger = logging.getLogger(name)
+		self._logger.setLevel(logging.INFO)
+
+		# create console handler and set level to debug
+		ch = logging.StreamHandler()
+		ch.setLevel(logging.INFO)
+
+		# create formatter
+		formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+		# add formatter to ch
+		ch.setFormatter(formatter)
+
+		# add ch to logger
+		self._logger.addHandler(ch)
+		return
+
+	"""
+	Main logging functions
+	"""
+	def debug(self, message):
+		self._logger.debug(message)
+
+	def info(self, message):
+		self._logger.info(message)
+
+	def warning(self, message):
+		self._logger.warning(message)
+
+	def warn(self, message):
+		self._logger.warn(message)
+
+	def error(self, mesaage):
+		self._logger.error(message)
 	
-	# create logger
-	logger = logging.getLogger(name)
-	logger.setLevel(logging.INFO)
+	def critical(self, message):
+		self._logger.critical(message)
 
-	# create console handler and set level to debug
-	ch = logging.StreamHandler()
-	ch.setLevel(logging.INFO)
+	def shutdown(self):
+		logging.shutdown()
+		return
 
-	# create formatter
-	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-	# add formatter to ch
-	ch.setFormatter(formatter)
-
-	# add ch to logger
-	logger.addHandler(ch)
-	return
-
-def shutdown():
-	logging.shutdown()
-	return
-
-def getLogger(name):
-	return configLogger(name)
+	def getLogger(self, name):
+		return self._logger
