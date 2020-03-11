1499d06d23d311bfe277d2e54df44ff840e3c38f
fivosts <fivos_ts@hotmail.com>
2020-03-11 00:03:21 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
 #TODO
+# setup(name='mrcrawley',
+#       version='0.0.1',
+#       description='Python utility to scrap data from websites',
+#       url='http://github.com/fivosts/MrCrawley',
+#       author='Foivos Tsimpourlas',
+#       author_email='fivos_ts@hotmail.com',
+#       license='MIT',
+#       packages=['mrcrawley'],
+#       install_requires=[
+#           'scrapy',
+#           'logging'#,
+#           # 'lazytools'
+#       ],
+#       zip_safe=True)
