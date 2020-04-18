611e9af048df98069bf885f1a7b1f12d46b904db
fivosts <fivos_ts@hotmail.com>
2020-04-18 16:16:45 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name = 'eupy',
-      version = '1.0.0',
+      version = '1.0.1',
       description = 'Python set of utils and libraries',
       url = 'http://github.com/fivosts/eupy',
       author = 'Foivos Tsimpourlas',
