68dbf60b2184a2fa732a5fb69882633657407a8e
fivosts <fivos_ts@hotmail.com>
2020-04-26 16:22:16 +0100

    Update profiler

--- eupy/native/profiler.py
+++ eupy/native/profiler.py
@@ -2,17 +2,21 @@
 import os
 import logging
 
-os.path.basename(frame.f_code.co_filename)
+# os.path.basename(frame.f_code.co_filename)
 
 def tracefunc(frame, event, arg):
 
-      if event == "call":
-          indent[0] += 2
-          print("-" * indent[0] + "> call function", frame.f_code.co_name)
-      elif event == "return":
-          print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
-          indent[0] -= 2
-      return tracefunc
+	print(frame)
+	print(frame.f_code)
+	print(frame.f_locals)
+	print(frame.f_back)
+	print(event)
+	print(arg)
+	if event == "call":
+		print("-" + "> call function", frame.f_code.co_name)
+	elif event == "return":
+		print("<" + "-", "exit function", frame.f_code.co_name)
+	return tracefunc
 
 # import sys
 # sys.setprofile(tracefunc)
